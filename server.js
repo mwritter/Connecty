46a17d1b85e2b13068367185172c2e05f8212f9e
Matthew Ritter <mritter2283@gmail.com>
2018-10-27 16:04:23 -0400

    fixed untracked files

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const app = express();
+
+//DB config
+const db = require("./config/keys").mongoURI;
+
+//Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("hello"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
