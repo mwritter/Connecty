8c1cbe23037bade42dbee657b57170235a9fde5d
Matthew Ritter <mritter2283@gmail.com>
2018-11-08 07:23:23 -0500

    Added delete post to api, only owner can delete their own posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,7 @@ const passport = require("passport");
 // Load user and post models
 const User = require("../../models/User");
 const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
 
 // Load input validation
 const validatePostInput = require("../../validation/post");
@@ -61,6 +62,30 @@ router.get("/:post_id", (req, res) => {
     );
 });
 
-module.exports = router;
+// @route   DELETE /api/posts/:post_id
+// @desc    Delete single post by post_id
+// @access  Private
+router.delete(
+  "/:post_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.post_id)
+        .then(post => {
+          // check for post owner because you don't want someone to delete posts created by someone else
+
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: "User not authorized" });
+          }
+
+          //Delete
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
+  }
+);
 
 module.exports = router;
