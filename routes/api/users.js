5a5b35636b4fdec46a50a830afff16f336435e1e
Matthew Ritter <mritter2283@gmail.com>
2018-11-19 16:19:06 -0500

    Added test route back to my API, also created frontend client for web app

--- routes/api/users.js
+++ routes/api/users.js
@@ -13,6 +13,11 @@ const validateLoginInput = require("../../validation/login");
 // Load User Model
 const User = require("../../models/User");
 
+// @route   GET /api/users/test
+// @desc    Tests users route
+// @access  Public
+router.get("/test", (req, res) => res.json({ msg: "Users works." })); // The route implies /api/users/test
+
 // @route   GET /api/users/register
 // @desc    Register user
 // @access  Public
