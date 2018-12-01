9aa45bd221d95aa0161f47fe9907f08b860a961c
Matthew Ritter <mritter2283@gmail.com>
2018-12-01 10:07:33 -0500

    Added imports to create/add redux store to our app

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,40 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+
+const middleware = [thunk];
+const store = createStore(
+  () => [], //empty store
+  {},
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
-            <Route path="/landing" component={Landing} />
-            <Route exact path="/" component={Landing} />
-          </Switch>
-          <Footer />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Navbar />
+            <Switch>
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+              <Route path="/landing" component={Landing} />
+              <Route exact path="/" component={Landing} />
+            </Switch>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
